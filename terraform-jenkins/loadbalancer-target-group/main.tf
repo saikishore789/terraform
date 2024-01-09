e3f1946619485a3f4d9a91f919e78df82bd74ded
sai kishore reddy Rachumalla <69140523+saikishore789@users.noreply.github.com>
2024-01-09 19:40:24 +0000

    created hosted zone and certificate manager

--- terraform-jenkins/loadbalancer-target-group/main.tf
+++ terraform-jenkins/loadbalancer-target-group/main.tf
@@ -14,6 +14,7 @@ resource "aws_lb_target_group" "dev_proj_1_lb_target_group" {
   name = var.lb_target_group_name
   port = var.lb_target_group_port
   protocol = var.lb_target_group_protocol
+  vpc_id = var.vpc_id
 
   health_check {
     path = "/login"
